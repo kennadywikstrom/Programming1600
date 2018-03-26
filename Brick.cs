0a660f4f2673bf8c6ca51805c37ca28a4e260e4f
Kennady Wikstrom <35277064+kennadywikstrom@users.noreply.github.com>
2018-03-26 09:16:41 -0600

    Uploaded Brick Breaker Build

---
+++ Brick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+
+    public int health;
+    public Sprite[] sprites;
+
+    private void Awake()
+    {
+        GameManager.brickCount++;
+        print(GameManager.brickCount);
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
+
+        if (health <= 0)
+        {                   //if our health gets to zero
+            GameManager.brickCount--;
+            print(GameManager.brickCount);
+            if (GameManager.brickCount == 0)
+            {
+                FindObjectOfType<GameManager>().LoadNextLevel();
+            }
+            Destroy(gameObject);            //then destroy this object
+        }
+
+
+    }
+
+
+}
---
+++ Floor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+
+    public GameManager theGameManager;
+
+    void Start()
+    {
+        theGameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Game Over!!!!");
+        theGameManager.LoadLevel("GameOver");
+    }
+
+}
+
