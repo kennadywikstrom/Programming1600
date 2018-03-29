9b218fd7725d682b437f32a98fc3748862c5f9df
Kennady Wikstrom <35277064+kennadywikstrom@users.noreply.github.com>
2018-03-29 11:27:30 -0600

    Added Scripts.

---
+++ BlockBreaker/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public AudioClip hitSound;
+    public Vector2 startVector;
+
+    private void Start() {
+        GetComponent<Rigidbody2D>().AddForce(startVector, ForceMode2D.Force);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        AudioSource.PlayClipAtPoint(hitSound, new Vector3(0,0,0));
+    }
+
+
+}
---
+++ BlockBreaker/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    public bool autoPlay;
+    private GameObject ball;
+
+    private void Start() {
+        ball = FindObjectOfType<Ball>().gameObject;
+    }
+
+    void Update () {
+        //get current paddle position
+        Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
+
+
+        if (autoPlay) {
+            paddlePosition.x = ball.transform.position.x;
+        } else {
+            //get mouse horizontal position
+            float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16 - 8;
+
+            paddlePosition.x = mousePositionInBlocks;
+        }
+
+        //set this paddle object to saved position
+        gameObject.transform.position = paddlePosition;
+	}
+}
