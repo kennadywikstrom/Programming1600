069b7e9297ec2f553d44ef6a71d57aa542bc9064
Kennady Wikstrom <35277064+kennadywikstrom@users.noreply.github.com>
2018-03-30 09:58:06 -0600

    Add files via upload

---
+++ BlockBreaker/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    public static int brickCount;
+
+    void Awake()
+    {
+        //Singleton
+        if (instance == null)
+        {             //if instance is not assigned
+            instance = this;                //then assign instance to this object
+        }
+        else if (instance != this)
+        {      //if this object is not THE instance
+            Destroy(this.gameObject);       //then destroy this object
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadLevel(string level)
+    {
+        brickCount = 0;
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadNextLevel()
+    {
+        brickCount = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+
+}
