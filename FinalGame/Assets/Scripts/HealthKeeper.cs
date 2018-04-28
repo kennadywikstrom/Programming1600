233c84b5cc1253ed93b89b0648b5a7aadc1fe834
Kennady Wikstrom <35277064+kennadywikstrom@users.noreply.github.com>
2018-04-27 20:36:40 -0600

    HealthKeeper

---
+++ FinalGame/Assets/Scripts/HealthKeeper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthKeeper : MonoBehaviour
+{
+
+    public static int health = 100;
+    private Text myText;
+
+    void Start()
+    {
+        myText = GameObject.Find("Score").GetComponent<Text>();
+    }
+
+    public void Health(int points)
+    {
+        Debug.Log("Lost Health");
+        health -= points;
+        myText.text = health.ToString();
+    }
+
+    public static void Reset()
+    {
+        health = 100;
+    }
+}
