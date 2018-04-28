3f9a2391ac058fd8898c94dfa79f1a3cd791846b
Kennady Wikstrom <35277064+kennadywikstrom@users.noreply.github.com>
2018-04-27 20:37:08 -0600

    LevelManager

---
+++ FinalGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(name);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+}
